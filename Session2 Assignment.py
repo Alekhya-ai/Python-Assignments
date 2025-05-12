6119f56f642bee9aa72248c45104eb872d2bc5d9
Alekhya-ai <alekhya.chowdary@pivotree.com>
2025-05-13 00:06:03 +0530

    Add files via upload
    Second Assignment

---
+++ Session2 Assignment.py
@@ -0,0 +1,12 @@
+# py_s2a1_adding_two_numbers -You’re building a simple billing system for a grocery store. A customer picks two items and the cashier needs to calculate the total cost by adding the prices of the two items.
+
+# Your task is to create a program that:
+# Takes input of two item prices from the user.
+# Adds the two numbers.
+# Displays the total amount the customer needs to pay.
+
+print("Welcome to the Billing Counter of Target Store")
+item1=float(input("enter the cost of first item: "))
+item2=float(input("enter the cost of second item: "))
+TotalCost: object= item1+item2
+print(TotalCost)
